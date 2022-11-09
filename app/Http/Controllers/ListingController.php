485b7c5cf49286d4371fe74d87e9a264a2d36dc2
yeongjun_kim <yeongjun0925@gmail.com>
2022-11-09 17:12:35 +0900

    Tag Filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,10 +8,10 @@ use App\Models\Listing;
 class ListingController extends Controller
 {
     // Show all listings
-    public function index()
+    public function index(Request $request)
     {
         return view('listings.index', [
-            'listings' => Listing::all(),
+            'listings' => Listing::latest()->filter(request(['tag']))->get()
         ]);
     }
 
