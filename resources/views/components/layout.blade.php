07f907d5c0d489d19c9fb7500031736aaa0ebe17
yeongjun_kim <yeongjun0925@gmail.com>
2022-11-21 21:47:27 +0900

    User Logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -44,6 +44,14 @@
                         <i class="fa-solid fa-gear"></i> Manage Listing
                     </a>
                 </li>
+                <li>
+                    <form action="/logout" class="inline" method="POST">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i> Logout
+                        </button>
+                    </form>
+                </li>
                 @else
                 <li>
                     <a href="/register" class="hover:text-laravel">
