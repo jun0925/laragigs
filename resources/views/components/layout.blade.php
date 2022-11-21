607cd92a7110501c4eb7fc52a56e9388c91cbe92
yeongjun_kim <yeongjun0925@gmail.com>
2022-11-21 21:40:53 +0900

    auth links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,6 +33,18 @@
                 <img class="w-24" src="{{ asset('images/logo.png')}}" alt="" class="logo" />
             </a>
             <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+                <li>
+                    <span class="font-bold uppercase">
+                        Welcome {{auth()->user()->name}}
+                    </span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel">
+                        <i class="fa-solid fa-gear"></i> Manage Listing
+                    </a>
+                </li>
+                @else
                 <li>
                     <a href="/register" class="hover:text-laravel">
                         <i class="fa-solid fa-user-plus"></i> Register
@@ -43,6 +55,7 @@
                         <i class="fa-solid fa-arrow-right-to-bracket"></i> Login
                     </a>
                 </li>
+                @endauth
             </ul>
         </nav>
 
